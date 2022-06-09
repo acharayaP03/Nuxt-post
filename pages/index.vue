fa8c4c5c1bd28a7cfa08f86cb3820fb7af021969
Trishten ac <trishtenac@trishtens-mbp.lan>
2022-06-09 22:07:59 +1000

    post item

--- pages/index.vue
+++ pages/index.vue
@@ -18,10 +18,9 @@
             </div>
           </div>
         </div>
-        <Post />
+        <PostItem :posts="postsData"/>
       </div>
     </section><!-- End Blog Section -->
-
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
@@ -51,9 +50,66 @@
 
 <script>
 import NavBar from '~/components/NavBar.vue';
-import Post from '~/components/Posts/Post.vue';
+import PostItem from '~/components/Posts/PostItem.vue';
 export default {
     name: "IndexPage",
-    components: { NavBar, Post }
+    components: { NavBar, PostItem },
+    data(){
+      return {
+          postsData:[
+              {
+                  _id: 'lkdsfkadhs234532naskldfh',
+                  category: 'Web Design',
+                  description: 'Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.',
+                  title: 'See more ideas about Travel',
+                  createdAt: new Date(),
+                  authorDetail: [
+                    {
+                      name: 'Prabhakar Ac',
+                      avatar: 'testimonial-2.jpg'
+                    }
+                  ],
+                  blogCover: [{
+                      imageUrl: 'post-3.jpg',
+                      altDescp: 'Web design cover pic'
+                  }]
+              },
+              {
+                  _id: 'adgfjdgbrewhs234532nafh',
+                  category: 'Web Design',
+                  description: 'Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.',
+                  title: 'See more ideas about Travel',
+                   createdAt: new Date(),
+                  authorDetail: [
+                    {
+                      name: 'Trishten Ac',
+                      avatar: 'testimonial-2.jpg'
+                    }
+                  ],
+                  blogCover: [{
+                      imageUrl: 'post-2.jpg',
+                      altDescp: 'Web design cover pic'
+                  }]
+              },
+              {
+                  _id: 'adgfjdgbrewhs234532nh',
+                  category: 'Travel',
+                  description: 'Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.',
+                  title: 'See more ideas about Travel',
+                  createdAt: new Date(),
+                  authorDetail: [
+                    {
+                      name: 'Lily Vaidya (Ac)',
+                      avatar: 'testimonial-4.jpg'
+                    }
+                  ],
+                  blogCover: [{
+                      imageUrl: 'post-1.jpg',
+                      altDescp: 'Web design cover pic'
+                  }]
+              }
+          ]
+      }
+    }
 }
 </script>
