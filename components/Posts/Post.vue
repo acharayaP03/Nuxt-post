3c619095d3ed52af53a29bdfaf89b0779dc015ce
Trishten ac <trishtenac@trishtens-mbp.lan>
2022-06-09 18:56:53 +1000

    seperate css

--- components/Posts/Post.vue
+++ components/Posts/Post.vue
@@ -104,5 +104,64 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+/*--------------------------------------------------------------
+    # Blog
+    --------------------------------------------------------------*/
+@media (min-width: 767px) {
+    .card-blog {
+        margin-bottom: 3rem;
+    }
+}
 
+.card-blog {
+    .card-body {
+        position: relative;
+    }
+
+    .card-category-box {
+        position: absolute;
+        text-align: center;
+        top: -16px;
+        left: 15px;
+        right: 15px;
+        line-height: 25px;
+        overflow: hidden;
+    }
+
+    .card-category {
+        display: inline-block;
+        color: #fff;
+        padding: 0 15px 5px;
+        overflow: hidden;
+        background-color: #0078ff;
+        border-radius: 4px;
+
+        .category {
+            color: #fff;
+            display: inline-block;
+            text-transform: uppercase;
+            font-size: 0.7rem;
+            letter-spacing: 0.1px;
+            margin-bottom: 0;
+        }
+    }
+    .card-title {
+        font-size: 1.3rem;
+        margin-top: 0.6rem;
+    }
+
+    .card-description {
+        color: #4e4e4e;
+    }
+
+    .post-author {
+        display: inline-block;
+    }
+    .post-date {
+        color: #4e4e4e;
+        display: inline-block;
+        float: right;
+    }
+
+}
 </style>
