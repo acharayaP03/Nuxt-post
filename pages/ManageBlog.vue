da10e4047253b4b84ffd793ee7aca4bd61cb22bf
Trishten ac <trishtenac@trishtens-mbp.lan>
2022-07-27 21:21:08 +1000

    css fix for manage blog

--- pages/ManageBlog.vue
+++ pages/ManageBlog.vue
@@ -8,28 +8,30 @@
       <section class="blog-wrapper sect-pt4" id="blog">
         <div class="container">
           <div class="row">
-            <div class="col-md-8">
+            <div class="col-md-12">
                 <SearchBar />
             </div>
-            <div class="col-md-4">
-              <div class="widget-sidebar sidebar-search">
-                <div class="sidebar-content">
-                  <div class="row">
-                    <div class="col-md-12">
-                      <button
-                        type="submit"
-                        class="button button-a button-big button-rouded"
-                      >
-                        Create new blog
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
+            <div class="row">
+                <div class="col-md-4 ml-auto">
+                    <div class="widget-sidebar sidebar-search">
+                        <div class="sidebar-content">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <button
+                                    type="submit"
+                                    class="button button-a button-big button-rouded"
+                                    >
+                                    Create new blog
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
           <div class="row">
-            <div class="col-md-8">
+            <div class="col-md-12">
               <div class="box-comments">
                 <div class="title-box-2">
                   <h4 class="title-comments title-left">List of blogs</h4>
@@ -57,11 +59,6 @@
                 </ul>
               </div>
             </div>
-            <div class="col-md-4">
-                <RecentPosts />
-                <ArchivedPosts />
-                <PostsTags />
-            </div>
           </div>
         </div>
       </section>
@@ -76,13 +73,10 @@
 import NavBar from '~/components/NavBar.vue'
 import HeroContainer from '~/components/HeroContainer.vue'
 import SearchBar from '~/components/SearchBar.vue'
-import RecentPosts from '~/components/Posts/RecentPosts.vue'
-import ArchivedPosts from '~/components/Posts/ArchivedPosts.vue'
-import PostsTags from '~/components/Posts/PostsTags.vue'
 import FooterComponent from '~/components/FooterComponent.vue'
 export default {
   name: 'IndexPage',
-  components: { NavBar, HeroContainer, SearchBar, RecentPosts, ArchivedPosts, PostsTags, FooterComponent },
+  components: { NavBar, HeroContainer, SearchBar, FooterComponent },
   data() {
     return {
       title: 'Here are the list of posts that are recently added.',
