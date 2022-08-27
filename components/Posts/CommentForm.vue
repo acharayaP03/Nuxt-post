88d7d9b8ef8e5b42b1627dc5183f720dcd63ab89
acharayaP03 <acharyaP03@gmail.com>
2022-08-28 09:52:20 +1000

    modal slot content and post forms

--- components/Posts/CommentForm.vue
+++ components/Posts/CommentForm.vue
@@ -7,57 +7,57 @@
             <div class="row">
             <div class="col-md-6 mb-3">
                 <div class="form-group">
-                <input
-                    id="inputName"
-                    type="text"
-                    class="form-control input-mf"
-                    placeholder="Name *"
-                    required
-                />
+                  <input
+                      id="inputName"
+                      type="text"
+                      class="form-control input-mf"
+                      placeholder="Name *"
+                      required
+                  />
                 </div>
             </div>
             <div class="col-md-6 mb-3">
                 <div class="form-group">
-                <input
-                    id="inputEmail1"
-                    type="email"
-                    class="form-control input-mf"
-                    placeholder="Email *"
-                    required
-                />
+                  <input
+                      id="inputEmail1"
+                      type="email"
+                      class="form-control input-mf"
+                      placeholder="Email *"
+                      required
+                  />
                 </div>
             </div>
             <div class="col-md-12 mb-3">
                 <div class="form-group">
-                <input
-                    id="inputUrl"
-                    type="url"
-                    class="form-control input-mf"
-                    placeholder="Website"
-                />
+                  <input
+                      id="inputUrl"
+                      type="url"
+                      class="form-control input-mf"
+                      placeholder="Website"
+                  />
                 </div>
             </div>
-            <div class="col-md-12 mb-3">
-                <div class="form-group">
-                <textarea
-                    id="textMessage"
-                    class="form-control input-mf"
-                    placeholder="Comment *"
-                    name="message"
-                    cols="45"
-                    rows="8"
-                    required
-                ></textarea>
-                </div>
-            </div>
-            <div class="col-md-12">
-                <button
-                type="submit"
-                class="button button-a button-big button-rouded"
-                >
-                Send Message
-                </button>
-            </div>
+              <div class="col-md-12 mb-3">
+                  <div class="form-group">
+                  <textarea
+                      id="textMessage"
+                      class="form-control input-mf"
+                      placeholder="Comment *"
+                      name="message"
+                      cols="45"
+                      rows="8"
+                      required
+                  ></textarea>
+                  </div>
+              </div>
+              <div class="col-md-12">
+                  <button
+                  type="submit"
+                  class="button button-a button-big button-rouded"
+                  >
+                  Send Message
+                  </button>
+              </div>
             </div>
         </form>
     </div>
@@ -67,7 +67,7 @@ export default {
     name: 'CommentFrom',
     data(){
         return{
-            
+
         }
     }
 
---
+++ components/Posts/CreatePostForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="form-comments">
+    <form class="form-mf">
+      <div class="row">
+        <div class="col-md-12 mb-3">
+          <div class="form-group">
+            <input
+              id="inputName"
+              type="text"
+              class="form-control input-mf p-4"
+              placeholder="Title *"
+              required
+            />
+          </div>
+        </div>
+        <div class="col-md-12 mb-3">
+          <div class="form-group">
+            <input
+              id="inputUrl"
+              type="text"
+              class="form-control input-mf p-4"
+              placeholder="Author"
+            />
+          </div>
+        </div>
+        <div class="col-md-12">
+          <div class="form-group">
+            <textarea
+              id="textMessage"
+              class="form-control input-mf p-4"
+              placeholder="Content *"
+              name="message"
+              cols="45"
+              rows="8"
+              required
+            ></textarea>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CreatePostForm"
+}
+</script>
+
+<style scoped>
+
+</style>
